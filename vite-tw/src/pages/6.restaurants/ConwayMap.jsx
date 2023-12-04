d2097b0ad0c49f82e6a4a70108b11a864235227c
Olivia <larsonog@hendrix.edu>
2023-12-04 15:16:52 -0600

    added address and urls for restaurants

--- vite-tw/src/pages/6.restaurants/ConwayMap.jsx
+++ vite-tw/src/pages/6.restaurants/ConwayMap.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { rList } from './List';
+import { Link } from 'react-router-dom'
 
-function ConwayMap() {
+function ConwayMap({rList}) {
   const conwayPosition = [35.0887, -92.4421];
   return (
     <MapContainer center={conwayPosition} zoom={15} style={{ height: '500px', width: '100%' }}>
@@ -14,7 +15,7 @@ function ConwayMap() {
       {/* Map through the restuarants locations and create markers with popups */}
       {rList.map((shop, index) => (
         <Marker key={index} position={shop.coordinates}>
-          <Popup>{shop.name}</Popup>
+          <Popup>{shop.name} <div className='small-empty-space'/> {shop.address} <div className='small-empty-space'/><Link to={shop.url}>{shop.name} Restaurant link</Link></Popup>
         </Marker>
       ))}
     </MapContainer>
--- vite-tw/src/pages/6.restaurants/Restaurants.jsx
+++ vite-tw/src/pages/6.restaurants/Restaurants.jsx
@@ -1,26 +1,35 @@
 import ConwayMap from "./ConwayMap"
-import {rList} from "./List"
+import { rList } from "./List"
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-crossorigin=""/>
+  integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+  crossorigin="" />
+
+function submitMoreR() {
+  let x = document.getElementById("moreRList").value;
+  document.getElementById("demo").innerHTML = x;
+}
 
 const Restaurants = () => {
 
-  
+
   return (
     <div>
       <h1>
         Restaurants
       </h1>
       <div className="row">
-      <ConwayMap rList = {rList}/>
-      <div className= "container">
-      {rList.map((shop, index) => (
-        <ul> {shop.name} </ul>
-      ))}
+        <ConwayMap rList={rList} />
+        <div className="border">x
+          <div className="container">
+            {rList.map((shop, index) => (
+              <ul> {shop.name} </ul>
+            ))}
+          </div>
+        </div>
       </div>
-      </div>
-    </div>
+ 
+    
+    </div >
   )
 }
 
