5573aea87ab97a5a01ce7e4fce496811d3869348
Olivia <larsonog@hendrix.edu>
2023-12-04 15:43:47 -0600

    Added links to address that takes you to gmaps

--- vite-tw/src/pages/6.restaurants/List.jsx
+++ vite-tw/src/pages/6.restaurants/List.jsx
@@ -1,14 +1,14 @@
   // Define an array of second-hand shop locations with coordinates and labels
  export  const rList = [
-    { name: 'Mikes Place ', coordinates: [35.08989334054979, -92.44034683223524], address: '808 Front St, Conway, AR 72032', url: 'https://mikesplaceconway.com' },
-    { name: 'BMB Creations Bakery ', coordinates: [35.090286049774704, -92.43920770524913], address:'812 Chestnut St, Conway, AR 72032', url: 'https://www.bmbcreations.com'},
-    { name: 'Cross Creek Sandwhich Shop ', coordinates: [35.09138311987305, -92.4389370322352], address: '1003 Oak St, Conway, AR 72032', url: 'https://crosscreeksandwichshop.com' },
-    { name: 'Hole in the Wall Cafe ', coordinates:[35.092414408624926, -92.43910811689258], address: '1016 Markham St, Conway, AR 72032', url: 'https://1486958.site123.me'},
-    { name: 'Bobs Grill ', coordinates: [35.091294813695306, -92.44031756418578], address:'1112 Oak St, Conway, AR 72032', url: 'https://www.facebook.com/profile.php?id=100054453522002'},
-    { name: 'Hollys Country Cooking ', coordinates:[35.07694781790386, -92.43402075922185], address: '116 S Harkrider St, Conway, AR 72032', address: 'https://www.hollyscountrycookin.com'},
-    { name: 'Stobys Restaurant ', coordinates:[35.08871857412287, -92.45412827456403], address: '805 Donaghey Ave, Conway, AR 72034',url:'https://stobys.com'},
-    { name: 'Shortys Bar-B-Q ', coordinates:[35.09376770122701, -92.43720266788422], address:'1101 Harkrider St, Conway, AR 72032', url:'https://www.facebook.com/ShortysinConway/'},
-    { name: 'Verona Italian Restaurant ', coordinates:[35.113072847886656, -92.42945097456317], address:'190 Skyline Dr, Conway, AR 72032', url:'https://www.facebook.com/VeronaConwayAr/'},
-    { name: 'The Rogue Roundabout ', coordinates:[35.09021098245866, -92.43927327641352], address: '804 Chestnut St, Conway, AR 72032', url:'https://www.therogueroundabout.com'}
+    { name: 'Mikes Place ', coordinates: [35.08989334054979, -92.44034683223524], address: '808 Front St, Conway, AR 72032', url: 'https://mikesplaceconway.com', locLink: 'https://maps.google.com/?ll=35.08989334054979,-92.44034683223524' },
+    { name: 'BMB Creations Bakery ', coordinates: [35.090286049774704, -92.43920770524913], address:'812 Chestnut St, Conway, AR 72032', url: 'https://www.bmbcreations.com', locLink: 'https://maps.google.com/?ll=35.090286049774704,-92.43920770524913'},
+    { name: 'Cross Creek Sandwhich Shop ', coordinates: [35.09138311987305, -92.4389370322352], address: '1003 Oak St, Conway, AR 72032', url: 'https://crosscreeksandwichshop.com', locLink:'https://maps.google.com/?ll=35.09138311987305,-92.4389370322352' },
+    { name: 'Hole in the Wall Cafe ', coordinates:[35.092414408624926, -92.43910811689258], address: '1016 Markham St, Conway, AR 72032', url: 'https://1486958.site123.me', locLink:'https://maps.google.com/?ll=35.092414408624926,-92.43910811689258'},
+    { name: 'Bobs Grill ', coordinates: [35.091294813695306, -92.44031756418578], address:'1112 Oak St, Conway, AR 72032', url: 'https://www.facebook.com/profile.php?id=100054453522002', locLink:'https://maps.google.com/?ll=35.091294813695306,-92.44031756418578'},
+    { name: 'Hollys Country Cooking ', coordinates:[35.07694781790386, -92.43402075922185], address: '116 S Harkrider St, Conway, AR 72032', address: 'https://www.hollyscountrycookin.com', locLink:'https://maps.google.com/?ll=35.07694781790386,-92.43402075922185'},
+    { name: 'Stobys Restaurant ', coordinates:[35.08871857412287, -92.45412827456403], address: '805 Donaghey Ave, Conway, AR 72034',url:'https://stobys.com', locLink: 'https://maps.google.com/?ll=35.08871857412287,-92.45412827456403'},
+    { name: 'Shortys Bar-B-Q ', coordinates:[35.09376770122701, -92.43720266788422], address:'1101 Harkrider St, Conway, AR 72032', url:'https://www.facebook.com/ShortysinConway/', locLink:'https://maps.google.com/?ll=35.09376770122701,-92.43720266788422'},
+    { name: 'Verona Italian Restaurant ', coordinates:[35.113072847886656, -92.42945097456317], address:'190 Skyline Dr, Conway, AR 72032', url:'https://www.facebook.com/VeronaConwayAr/', locLink:'https://maps.google.com/?ll=35.113072847886656,-92.42945097456317'},
+    { name: 'The Rogue Roundabout ', coordinates:[35.09021098245866, -92.43927327641352], address: '804 Chestnut St, Conway, AR 72032', url:'https://www.therogueroundabout.com', locLink: 'https://maps.google.com/?ll=35.09021098245866,-92.43927327641352'}
     
   ];
