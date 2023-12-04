9b3724db3d6f91875617fee35cb8913496f506e1
Olivia <larsonog@hendrix.edu>
2023-12-04 16:27:00 -0600

    Moved array to new file

---
+++ vite-tw/src/pages/8.walkInClinics/WList.jsx
@@ -0,0 +1,19 @@
+
+  // an array of walk-in clinic locations with coordinates and labels
+  export const walkInClinicsLoc = [
+    { name: 'Medcare Urgent Express', coordinates: [35.09176934180663, -92.43467766052645], locLink: 'https://maps.google.com/?ll=35.09176934180663,-92.43467766052645', address:'805 Oak St, Conway, AR 72032' },
+    { name: 'Primecare Medical Clinic', coordinates: [35.09220827463228, -92.43614751101053], locLink: 'https://maps.google.com/?ll=35.09220827463228,-92.43614751101053', address:'812 Oak St, Conway, AR 72032' },
+    { name: 'Conway Family Practice Clinic', coordinates: [35.09087158632688, -92.44303638483913], locLink: 'https://maps.google.com/?ll=35.09087158632688,-92.44303638483913', address:'919 Locust St, Conway, AR 72034' },
+    { name: 'Baptist Health Urgent Care- Conway North', coordinates:[35.110742608038734, -92.43901134435995], locLink: 'https://maps.google.com/?ll=35.110742608038734,-92.43901134435995', address:'1065 Skyline Dr, Conway, AR 72032'},
+    { name: 'Conway Regional Afterhours Clinic', coordinates: [35.08448471767092, -92.46562988668869], locLink: 'https://maps.google.com/?ll=35.08448471767092,-92.46562988668869', address:'437 Denison St Suite 2, Conway, AR 72034'},
+    { name: 'Baptist Health Urgent Care- Conway South', coordinates:[35.06732266774769, -92.42649868853822], locLink: 'https://maps.google.com/?ll=35.06732266774769,-92.42649868853822', address:'222 Dave Ward Dr, Conway, AR 72032'},
+    { name: 'Conway Regional Hendrix Medical Clinic', coordinates:[35.10308350572973, -92.4383924866887], locLink: 'https://maps.google.com/?ll=35.10308350572973,-92.4383924866887', address:'1700 Altus Suite 110, Conway, AR 72032'},
+    { name: 'College Park Family Clinic', coordinates:[35.084844449661574, -92.46373454435997], locLink: 'https://maps.google.com/?ll=35.084844449661574,-92.46373454435997', address:'2511 College Ave # A, Conway, AR 72034'},
+    { name: 'Conway Emergency Physicans', coordinates:[35.086129558367716, -92.45758619833217], locLink: 'https://maps.google.com/?ll=35.086129558367716,-92.45758619833217', address:'2302 College Ave, Conway, AR 72034'},
+    { name: 'Conway Regional Medical Clinic- Prince Street', coordinates:[35.091815097457165, -92.46040108483913], locLink: 'https://maps.google.com/?ll=35.091815097457165,-92.46040108483913', address:'1 Medical Ln, Conway, AR 72034'},
+    { name: 'PrimeCare Medical Clinic- Salem', coordinates:[35.091815097457165, -92.46040108483913], locLink: 'https://maps.google.com/?ll=35.091815097457165,-92.46040108483913', address:'605 Salem Rd, Conway, AR 72034'},
+    { name: 'Conway Regional Family Clinic', coordinates:[35.085699697142466, -92.47420409723976], locLink: 'https://maps.google.com/?ll=35.085699697142466,-92.47420409723976', address:'3025 Fountain Dr #100, Conway, AR 72034'},
+    { name: 'Conway Family Care', coordinates:[35.08596607243892, -92.49064564251043], locLink: 'https://maps.google.com/?ll=35.08596607243892,-92.49064564251043', address:'3650 College Ave, Conway, AR 72034'},
+    { name: 'Conway Medical Group- Conway Regional', coordinates:[35.084284328206145, -92.46561372901739], locLink: 'https://maps.google.com/?ll=35.084284328206145,-92.46561372901739', address:'437 Denison St, Conway, AR 72034'}
+
+  ];
--- vite-tw/src/pages/8.walkInClinics/WalkInClinics.jsx
+++ vite-tw/src/pages/8.walkInClinics/WalkInClinics.jsx
@@ -3,13 +3,14 @@ import ConwayMap from "./ConwayMap"
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
 integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
 crossorigin=""/>
+import { walkInClinicsLoc } from "./WList"
 
 const WalkInClinics = () => {
   return (
     <div>
       <h1>Walk-in Clinics
       </h1>
-      <ConwayMap/>
+      <ConwayMap walkInClinicsLoc={walkInClinicsLoc}/>
     </div>
   )
 }
