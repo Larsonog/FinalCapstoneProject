e7d2de037fa65106a395dd7ce9afbe041971908b
Olivia <larsonog@hendrix.edu>
2023-12-04 18:25:17 -0600

    added ideas + links to misc section of entertainment page

--- vite-tw/src/pages/4.entertainment/Entertainment.jsx
+++ vite-tw/src/pages/4.entertainment/Entertainment.jsx
@@ -15,10 +15,15 @@ const posts = [
     author: {}, // if I remove this, it gets angry
     image: './assets/Cadron.jpeg',
     list: <ul>
-      <ul>Cadron Ridge</ul>
-      <ul>Beaver Fork</ul> 
-      <ul> Hendrix Creek Preserve </ul> 
-      </ul>
+      <ul>Cadron Settlement Park</ul>
+      <ul>Beaver Fork Park</ul>
+      <ul> Hendrix Creek Preserve </ul>
+      <ul>Don Owen Sport Complex</ul>
+      <ul>Laurel Park</ul>
+      <ul>Tucker Creek Walking Trail</ul>
+      <ul>Jewel Moore Nature Reserve</ul>
+      <ul>Martin Luther King Jr. Square</ul>
+    </ul>
   },
 
   {
@@ -33,16 +38,16 @@ const posts = [
     author: {},
     image: './assets/rec.jpeg',
     list: <ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/adult-softball/' >  Adult Softball league </Link> </ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/adult-kickball/' >  Adult Kickball league </Link> </ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/adult-basketball/'>  Adult Basketball league </Link> </ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/adult-volleyball/' >  Adult Volleyball league </Link> </ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/disc-golf/' >  Disc Golf </Link> </ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-baseball/' >  Youth Baseball league </Link> </ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-softball/' >  Youth Softball league </Link> </ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-t-ball/' >  Youth T-ball </Link> </ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-flag-football/' >  Youth flag football </Link> </ul>
-    <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-softball/' >  Youth Fishing Derby </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/adult-softball/' >  Adult Softball league </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/adult-kickball/' >  Adult Kickball league </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/adult-basketball/'>  Adult Basketball league </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/adult-volleyball/' >  Adult Volleyball league </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/disc-golf/' >  Disc Golf </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-baseball/' >  Youth Baseball league </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-softball/' >  Youth Softball league </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-t-ball/' >  Youth T-ball </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-flag-football/' >  Youth flag football </Link> </ul>
+      <ul className='blue-underline'> <Link to='https://conwayarkansas.gov/parks/programs/youth-softball/' >  Youth Fishing Derby </Link> </ul>
     </ul>
   },
   {
@@ -57,10 +62,19 @@ const posts = [
     author: {},
     image: './assets/downtown.jpeg',
     list:
-     <ul>
-    <ul>grab a drink at Kings</ul>
-    <ul> check out Faulkner County public library</ul>
-    </ul>
+      <ul>
+        <ul className='blue-underline'> <Link to='https://www.kingslivemusic.com' > Grab a drink at Kings </Link> </ul>
+        <ul className='blue-underline'> <Link to='https://fcl.org' > Check out Faulkner County public library </Link> </ul>
+        <ul className='blue-underline'> <Link to='https://uca.edu/art/baum/' > Visit the Baum Gallery </Link> </ul>
+        <ul className='blue-underline'> <Link to='https://www.windgatemuseum.org' > Visit the Windgate Museum </Link> </ul>
+        <ul className='blue-underline'> <Link to='https://www.faulknercounty.org/faulknercountymuseum.org/' > Visit the Faulkner County Museum </Link> </ul>
+        <ul className='blue-underline'> <Link to='https://www.conwayfamilybowl.com' > Go Bowling at Conway Family Bowl </Link> </ul>
+        <ul className='blue-underline'> <Link to='https://www.climbconway.com' > Visit a climbing gym at Climb Conway </Link> </ul>
+        
+       
+        
+        
+      </ul>
   },
 
   // More posts...
@@ -78,66 +92,67 @@ export default function Entertainment() {
         </div>
 
 
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {posts.map((post) => (
-              <div className='post-border'>
-                <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
-                  <div>
-                    <img src={post.image} alt="" className="h-100 w-100 rounded bg-gray-50" style={{ height: 250, width: 400 }} />
-                  </div>
-                  <div className="flex items-center gap-x-4 text-xs">
-                    <time dateTime={post.datetime} className="text-gray-500">
-                      {post.date}
-                    </time>
-                    <a
-                      href={post.category.href}
-                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                    >
-                      {post.category.title}
-                    </a>
-                  </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {posts.map((post) => (
+            <div className='post-border'>
+              <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+                <div>
+                  <img src={post.image} alt="" className="h-100 w-100 rounded bg-gray-50" style={{ height: 250, width: 400 }} />
+                </div>
+                <div className="flex items-center gap-x-4 text-xs">
+                  <time dateTime={post.datetime} className="text-gray-500">
+                    {post.date}
+                  </time>
+                  <a
+                    href={post.category.href}
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >
+                    {post.category.title}
+                  </a>
+                </div>
 
-                  <div className="group relative">
-                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                      <a href={post.href}>
-                        <span className="absolute inset-0" />
-                        {post.title}
-                      </a>
-                    </h3>
-                    <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
-                  </div>
-                  <Popup trigger=
-                    {<div className='button-border'>
-                      <div className='h2'>
-                        <button> Click to see full list </button>
-                      </div>
-                    </div>}
-                    modal nested>
-                    {
-                      close => (
-                        <div className='modal'>
-                          <div className='content'>
-                            <h2>{post.title}</h2>
-                            <div className='small-empty-space' />
-                            <div className="container">
-                              {post.list}
-                            </div>
-                          </div>
-                          <div>
-                            <button onClick=
-                              {() => close()}>
-                            </button>
+                <div className="group relative">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <a href={post.href}>
+                      <span className="absolute inset-0" />
+                      {post.title}
+                    </a>
+                  </h3>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+                </div>
+                <Popup trigger=
+                  {<div className='button-border'>
+                    <div className='h2'>
+                      <button> Click to see full list </button>
+                    </div>
+                  </div>}
+                  modal nested>
+                  {
+                    close => (
+                      <div className='modal'>
+                        <div className='content'>
+                          <h2>{post.title}</h2>
+                          <div className='small-empty-space' />
+                          <div className="container">
+
+                            <p>{post.list}</p>
                           </div>
                         </div>
-                      )
-                    }
-                  </Popup>
+                        <div>
+                          <button onClick=
+                            {() => close()}>
+                          </button>
+                        </div>
+                      </div>
+                    )
+                  }
+                </Popup>
 
-                </article>
-              </div>
-            ))}
+              </article>
+            </div>
+          ))}
 
-          </div>
+        </div>
 
 
       </div>
